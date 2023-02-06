ca479a82ce57fa3e3757fcb0c67947b67ae2dc03
Adam <39442291+Aurathic@users.noreply.github.com>
2023-02-05 19:11:29 -0500

    Add pokemon.html templating

--- p2/flask_app/app.py
+++ p2/flask_app/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 from model import PokeClient
+
 app = Flask(__name__)
 
 poke_client = PokeClient()
 
-@app.route('/')
+
+@app.route("/")
 def index():
     """
     Must show all of the pokemon names as clickable links
@@ -12,22 +14,32 @@ def index():
     Check the README for more detail.
     """
     pokemon_list = poke_client.get_pokemon_list()
-    return render_template('index.html', pokemon_list=pokemon_list)
+    return render_template("index.html", pokemon_list=pokemon_list)
 
-@app.route('/pokemon/<pokemon_name>')
+
+@app.route("/pokemon/<pokemon_name>")
 def pokemon_info(pokemon_name):
     """
     Must show all the info for a pokemon identified by name
 
     Check the README for more detail
     """
-    return render_template('ability.html',pokemon_name=pokemon_name)
+    pokemon_info = poke_client.get_pokemon_info(pokemon_name)
+    return render_template(
+        "pokemon.html",
+        name=pokemon_info["name"],
+        weight=pokemon_info["weight"],
+        base_exp=pokemon_info["base_exp"],
+        moves=pokemon_info["moves"],
+        abilities=pokemon_info["abilities"],
+    )
 
-@app.route('/ability/<ability_name>')
+
+@app.route("/ability/<ability_name>")
 def pokemon_with_ability(ability_name):
     """
-    Must show a list of pokemon 
+    Must show a list of pokemon
 
     Check the README for more detail
     """
-    return render_template('ability.html',ability_name=ability_name)
+    return render_template("ability.html", ability_name=ability_name)
